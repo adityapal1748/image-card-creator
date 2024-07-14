1c8a63555969bc5e4c7097ad8e1df097325b363a
adityapal1748 <paladitya3112@gmail.com>
2024-07-14 15:41:52 +0530

    puppeteer changes

--- src/user-card-generator/services/image.service.ts
+++ src/user-card-generator/services/image.service.ts
@@ -2,7 +2,9 @@ import * as puppeteer from 'puppeteer';
 
 export class ImageService {
   async htmlToImage(html: string): Promise<Buffer> {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(html);
     const imageBuffer = await page.screenshot({ type: 'png',fullPage:true });
