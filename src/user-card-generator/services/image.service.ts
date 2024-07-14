19a6aa1562a36ac57323d6a7935167c539bc66ab
adityapal1748 <paladitya3112@gmail.com>
2024-07-14 15:47:47 +0530

    pup issue fixing

--- src/user-card-generator/services/image.service.ts
+++ src/user-card-generator/services/image.service.ts
@@ -2,9 +2,11 @@ import * as puppeteer from 'puppeteer';
 
 export class ImageService {
   async htmlToImage(html: string): Promise<Buffer> {
+    console.log('Launching browser...');
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
+    console.log('Browser launched');
     const page = await browser.newPage();
     await page.setContent(html);
     const imageBuffer = await page.screenshot({ type: 'png',fullPage:true });
